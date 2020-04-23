36eb408c7bb5cf3593c464766ebe4e6a007da439
zhangsan <zhangsan@qq.com>
2020-04-23 13:17:16 +0800

    这是从远程仓库下载后，第一次更新代码

--- idea-git-test/src/test/Test.java
+++ idea-git-test/src/test/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("你是谁");
         System.out.println("我是谁");
+        System.out.println("你是山东大李逵");
     }
 
 }
